39737a87d338cb129e33709daa6ec3d39b45b769
John McGill <john.mcgill@orvia.org.uk>
2026-09-24 23:39:44 +0100

    Serve Morrow showcase film from uploaded media

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  poweredByHeader: false
+  poweredByHeader: false,
+  async rewrites() {
+    return [
+      {
+        source: '/media/business/morrow-demo.mp4',
+        destination: 'https://raw.githubusercontent.com/ORVIA-Oversight/orvia-web-showcase/fa7811c0201e7a62aa1c41d429b990481fe8f27b/Create_a_second_cinematic_p%20(2).mp4'
+      }
+    ]
+  }
 }
 export default nextConfig
